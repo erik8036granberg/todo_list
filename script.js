c4eb10364dc86e22a3049f89f3b3260016fa2e32
Erik Granberg <36399140+erik8036granberg@users.noreply.github.com>
2019-03-07 17:12:06 +0100

    Json import of testdata ok

--- script.js
+++ script.js
@@ -5,4 +5,24 @@ window.addEventListener("DOMContentLoaded", init);
 
 function init() {
   console.log("init");
+  get();
+}
+
+// getJson
+function get() {
+  console.log("get");
+
+  fetch("https://todolist-f2b2.restdb.io/rest/todoitems", {
+      method: "get",
+      headers: {
+        "Content-Type": "application/json; charset=utf-8",
+        "x-apikey": "5c813fa1cac6621685acbc7f",
+        "cache-control": "no-cache"
+      }
+    })
+    // Get data - format as jason
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+    });
 }
