d0f66622175e38960b5b30a5e6aec5cad61895fb
Erik Granberg <36399140+erik8036granberg@users.noreply.github.com>
2019-03-08 23:26:38 +0100

    databasetest

--- script.js
+++ script.js
@@ -14,18 +14,18 @@ function init() {
 
 function getItems() {
   console.log("getItems");
-
   fetch("https://todolist-f2b2.restdb.io/rest/todoitems?metafields=true", {
-    method: "get",
-    headers: {
-      "Content-Type": "application/json; charset=utf-8",
-      "x-apikey": "5c813fa1cac6621685acbc7f",
-      "cache-control": "no-cache"
-    }
-  })
+      method: "get",
+      headers: {
+        "Content-Type": "application/json; charset=utf-8",
+        "x-apikey": "5c813fa1cac6621685acbc7f",
+        "cache-control": "no-cache"
+      }
+    })
     //   format as jason & send to sort
     .then(res => res.json())
     .then(data => {
+      console.log(data);
       items = data;
       sortItems(items);
     });
@@ -33,18 +33,18 @@ function getItems() {
 
 function postItem(newItem) {
   console.log("postItem");
-
   fetch("https://todolist-f2b2.restdb.io/rest/todoitems", {
-    method: "post",
-    body: JSON.stringify(newItem),
-    headers: {
-      "Content-Type": "application/json; charset=utf-8",
-      "x-apikey": "5c813fa1cac6621685acbc7f",
-      "cache-control": "no-cache"
-    }
-  })
+      method: "post",
+      body: JSON.stringify(newItem),
+      headers: {
+        "Content-Type": "application/json; charset=utf-8",
+        "x-apikey": "5c813fa1cac6621685acbc7f",
+        "cache-control": "no-cache"
+      }
+    })
     .then(res => res.json())
     .then(data => {
+      console.log(data);
       items.push(data);
       sortItems(items);
     });
@@ -52,33 +52,38 @@ function postItem(newItem) {
 
 function deleteItem(id) {
   console.log("deleteItem");
-
   fetch("https://todolist-f2b2.restdb.io/rest/todoitems/" + id, {
-    method: "delete",
-    headers: {
-      "Content-Type": "application/json; charset=utf-8",
-      "x-apikey": "5c813fa1cac6621685acbc7f",
-      "cache-control": "no-cache"
-    }
-  })
+      method: "delete",
+      headers: {
+        "Content-Type": "application/json; charset=utf-8",
+        "x-apikey": "5c813fa1cac6621685acbc7f",
+        "cache-control": "no-cache"
+      }
+    })
     .then(res => res.json())
-    .then(data => {});
+    .then(data => {
+      console.log(data);
+    });
+
 }
 
-function sortItems(data) {
+function sortItems(items) {
   console.log("sortItems");
-
-  // sort by _created meta tag
-  data.sort(function(a, z) {
-    if (a._created < z._created) {
-      return 1;
-    } else {
-      return -1;
-    }
-  });
-  // clear content from DOM
-  document.querySelector("[data-container]").innerHTML = "";
-  data.forEach(showItems);
+  if (items.length === 0) {
+    document.querySelector("[data-container]").innerHTML = `
+    <div class="no_tasks">Nice! All tasks are done...</div>
+    `;
+  } else {
+    items.sort(function (a, z) {
+      if (a._created < z._created) {
+        return 1;
+      } else {
+        return -1;
+      }
+    });
+    document.querySelector("[data-container]").innerHTML = "";
+    items.forEach(showItems);
+  }
 }
 
 function showItems(item) {
@@ -89,6 +94,7 @@ function showItems(item) {
   clone.querySelector("[data-details]").textContent = item.details;
   clone.querySelector("[data-id]").dataset.id = item._id;
   clone.querySelector("[data-delete]").addEventListener("click", e => {
+    e.preventDefault();
     deleteItemModal(item.title, item.details, item._id);
   });
   // clone.
@@ -114,19 +120,20 @@ function addItemModal() {
     <form id="itemForm">
     <h2>Add new task</h2>
     <label for=" title">Title</label>
-    <input type="text" name="title" id="title" placeholder="Bananas" required><br>
+    <input type="text" name="title" id="title" placeholder="What to do?" required><br>
     <label for="detalis">Details</label>
-    <input type="text" name="details" id="details" placeholder="The yellow ones" required><br><br>
-    <button type="submit" name="submit">Submit</button>
+    <input type="text" name="details" id="details" placeholder="Some more stuff about it..." required><br><br>
+    <button type="submit" name="submit">Add</button>
     <button type="reset" data-delete_cancel>Cancel</button>
     </form>`;
 
   document.querySelector("#itemForm").addEventListener("submit", e => {
-    console.log("submit");
     e.preventDefault();
-    itemForm.elements.submit.disabled = true;
+    console.log("submit");
+
     const newTitle = itemForm.elements.title.value;
     const newDetails = itemForm.elements.details.value;
+
     newItem = {
       title: newTitle,
       details: newDetails
@@ -143,20 +150,27 @@ function addItemModal() {
 
 function deleteItemModal(title, details, id) {
   console.log("deleteItemModal");
+  console.log(id);
   document.querySelector("#modal").classList.add("show");
   document.querySelector("#close").addEventListener("click", closeModal);
 
   document.querySelector("#modal_content").innerHTML = `
-    <p class="bold">All done?:</p>
+    <p class="bold">Really done with this?</p>
     <p>${title}</p>
     <p>${details}</p>
-    <button data-delete_ok>Yes</button>
+    <button data-delete_ok>Absolutly</button>
     <button data-delete_cancel>Not yet</button>
     `;
   document.querySelector("[data-delete_ok]").addEventListener("click", e => {
     event.preventDefault();
+    items = items.filter(function (item) {
+      return item.id != id;
+    });
+    console.log("Is id gone now?")
+    console.log(items);
     document.querySelector("[data-id='" + id + "']").remove();
     deleteItem(id);
+    console.log(id);
     closeModal();
   });
   document
